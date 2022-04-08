b26881ee88738abcedf98454f1e4c8622d6a2935
Simon Beddig <simon.beddig@googlemail.com>
2022-04-08 19:57:03 +0200

    add preconditions

---
+++ bootstrap/scripts/pre-conditions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+ENVS="AWS_ACCOUNT_ID AWS_PROFILE AWS_REGION"
+TOOLS="aws cdk"
+
+check_env_variables() {
+  for env in $ENVS; do
+    if [[ -z "${!env}" ]]; then
+      echo "Variable $env not set."
+      exit 1
+    fi
+  done
+}
+
+check_tools() {
+  for tool in $TOOLS; do
+    if ! $tool --version &>/dev/null; then
+      echo "$tool not installed."
+    fi
+  done
+}
+
+check_env_variables
+check_tools
